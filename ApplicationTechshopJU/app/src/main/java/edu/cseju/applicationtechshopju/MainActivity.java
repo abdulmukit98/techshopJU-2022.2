99b5092ec57fced2b9a3c7b901629cecd62ab8e3
abdulmukit98 <mdabdulmukit98@gmail.com>
2022-07-21 11:15:53 +0600

    fine tune

--- ApplicationTechshopJU/app/src/main/java/edu/cseju/applicationtechshopju/MainActivity.java
+++ ApplicationTechshopJU/app/src/main/java/edu/cseju/applicationtechshopju/MainActivity.java
@@ -3,14 +3,32 @@ package edu.cseju.applicationtechshopju;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+    Button lgBtnSignIn;
+    EditText lgEdtMail, lgEdtPass;
+    TextView lgTvCreateAcc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        lgTvCreateAcc = findViewById(R.id.lgTvCreateAcc);
+        lgEdtMail = findViewById(R.id.lgEdtMail);
+        lgEdtPass = findViewById(R.id.lgEdtPassword);
+        lgBtnSignIn = findViewById(R.id.lgBtnSignIn);
+
+
     }
 }
+
+
+
